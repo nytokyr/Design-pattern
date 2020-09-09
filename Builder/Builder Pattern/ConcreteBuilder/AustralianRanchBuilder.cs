dea5ff38215f47a098b997047fe25d4bbe8243c8
ny toky <22963493+nytokyr@users.noreply.github.com>
2020-09-09 22:24:35 +0200

    change path
    * do rename
    
    * change path

---
+++ Builder/Builder Pattern/ConcreteBuilder/AustralianRanchBuilder.cs
@@ -0,0 +1,36 @@
+﻿namespace Builder_Pattern.ConcreteBuilder
+{
+    public class AustralianRanchBuilder : HouseBuilder
+    {
+        House _house = new House();
+        internal override void SetRooms()
+        {
+            _house.Room = 4;
+        }
+
+        internal override void SetGarage()
+        {
+            _house.Garage = 3;
+        }
+
+        internal override void SetWindow()
+        {
+            _house.Window = 5;
+        }
+
+        internal override void SetDoors()
+        {
+            _house.Door = 2;
+        }
+
+        internal override void SetShape()
+        {
+            _house.Shape = RanchShape.U;
+        }
+
+        public House Build()
+        {
+            return _house;
+        }
+    }
+}
